fc59bb995b4071ce1abb85ddc2c7382384d1910c
Marcos Max Forosteski da Silva <max.silva@adami.com.br>
2024-11-07 16:56:16 -0300

    iniciar config do modo claro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,43 +2,48 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="navbar">
-            <nav className="bg-gray-50 usu-bg-nav">
-                <div className="max-w-screen-xl px-4">
-                    <div className="flex items-center">
-                        <ul className="flex flex-row items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-lg">
-                            <li>
-                                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-                                    <a className="flex items-center space-x-3 rtl:space-x-reverse">
-                                        <Image
-                                            src="/LinceY-logo.webp"
-                                            alt="Lincey Logo"
-                                            width={60}
-                                            height={60}
-                                            style={{ borderRadius: "50px" }}
-                                        />
-                                    </a>
-                                </div>
-                            </li>
-                            <li>
-                                <a className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <a className="text-gray-900 dark:text-white hover:underline">Sobre mim</a>
-                            </li>
-                            <li>
-                                <a className="text-gray-900 dark:text-white hover:underline">Serviços</a>
-                            </li>
-                            <li>
-                                <a className="text-gray-900 dark:text-white hover:underline">Projetos</a>
-                            </li>
-                            <li>
-                                <a className="text-gray-900 dark:text-white hover:underline">Contatos</a>
-                            </li>
-                        </ul>
+        <div className="main">
+            <div className="navbar">
+                <nav className="bg-gray-50 usu-bg-nav">
+                    <div className="max-w-screen-xl px-4">
+                        <div className="flex items-center">
+                            <ul className="flex flex-row items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-lg">
+                                <li>
+                                    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                                        <a className="flex items-center space-x-3 rtl:space-x-reverse">
+                                            <Image
+                                                src="/LinceY-logo.webp"
+                                                alt="Lincey Logo"
+                                                width={60}
+                                                height={60}
+                                                style={{ borderRadius: "50px" }}
+                                            />
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                                </li>
+                                <li>
+                                    <a className="text-gray-900 dark:text-white hover:underline">Sobre mim</a>
+                                </li>
+                                <li>
+                                    <a className="text-gray-900 dark:text-white hover:underline">Serviços</a>
+                                </li>
+                                <li>
+                                    <a className="text-gray-900 dark:text-white hover:underline">Projetos</a>
+                                </li>
+                                <li>
+                                    <a className="text-gray-900 dark:text-white hover:underline">Contatos</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
+            <div className="container">
+                .
+            </div>
         </div>
     );
 }
