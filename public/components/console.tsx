5d55c2af6856010a7cb33215ce493ce80dff61fb
Marcos Max Forosteski da Silva <max.silva@adami.com.br>
2024-11-27 11:42:25 -0300

    console pos relative

--- public/components/console.tsx
+++ public/components/console.tsx
@@ -7,21 +7,19 @@ export default function Console() {
     useEffect(() => {
         StringTypistSimulator("console-line",["console.log('Hello, my name is Max');",
                                                 "One day per Day[day];",
-                                                "UI/UX are so underated bruh;",
-                                                "- Speaks in Portuguese, English and Leetcode",
+                                                "UI/UX are so underrated bruh;",
+                                                "Speaks in Portuguese, English and Leetcode",
                                                 "System.out.println('A mug of happiness');"
                                             ])
     });
     return (
         <div className="console">
-            <div className="header absolute flex items-center text-5xl rounded-md">
+            <div className="header relative flex items-center text-5xl rounded-mdz-0 z-0">
                 <div className="console-line flex">
                     <p> &gt; </p><h1 className="z-0 font-semibold" id="console-line"></h1><div className="cursor"></div>
                 </div>
             </div>
-            <div className="header" >
-                Lorem ipsum dolor sit amet consectetur
-            </div >
+            
         </div>
 
     )
